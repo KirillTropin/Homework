8b2e2b2ee7ffa8f159b1cad6dd0bd6352949079c
KirillTropin <60031338+KirillTropin@users.noreply.github.com>
2023-05-04 21:54:13 +0500

    Depth First Search (DFS)

---
+++ src/main/java/org/example/Graph.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Graph {
+    private int numVertices;
+    private LinkedList<Integer> adjLists[];
+    private boolean visited[];
+
+    Graph(int vertices) {
+        numVertices = vertices;
+        adjLists = new LinkedList[vertices];
+        visited = new boolean[vertices];
+
+        for (int i = 0; i < vertices; i++)
+            adjLists[i] = new LinkedList<Integer>();
+    }
+
+    void addEdge(int src, int dest) {
+        adjLists[src].add(dest);
+    }
+
+    void DFS(int vertex) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        Iterator ite = adjLists[vertex].listIterator();
+        while (ite.hasNext()) {
+            int adj = (Integer) ite.next();
+            if (!visited[adj])
+                DFS(adj);
+        }
+    }
+}
